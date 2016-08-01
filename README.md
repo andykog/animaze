b36a3574402f8dca523a195327aed058cf42d45f
Andrey Kogut <andrey.kogut@uvoteam.com>
2016-08-01 10:06:02 +0300

    README

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ Supersmall animation library. [AMD is supported.]
 import animaze from 'animaze';
 
 const animazeOpacity = () => animaze({
-    onTick: { v => myDiv.style.opacity = v; }, // v is varied from 0 to 1
-                                               // during animation
+    tick: v => { myDiv.style.opacity = v; }, // v is varied from 0 to 1
+                                             // during animation
 });
 
 const animazeScroll = () =>  animaze({
-    onTick: { window.scrollTo(0, 200 * v) },
+    tick: v => window.scrollTo(0, 200 * v),
     duration: 500, // default is 240
     ease: k => 0.5 * (1 - Math.cos(Math.PI * k)), // this is default one
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-export default ({ onTick, duration = 240, ease = k => 0.5 * (1 - Math.cos(Math.PI * k)) }) => {
+export default ({ tick, onTick, duration = 240, ease = k => 0.5 * (1 - Math.cos(Math.PI * k)) }) => {
+  tick = tick || onTick;
   const startTime = window.performance && window.performance.now() || Date.now();
   let frame;
   return new Promise(resolve => {
     if (!window.requestAnimationFrame) {
-      onTick(1);
+      tick(1);
       resolve();
       return;
     }
     const step = () => {
-      frame = window.requestAnimationFrame(() => step(onTick));
+      frame = window.requestAnimationFrame(() => step(tick));
       const time = window.performance && window.performance.now() || Date.now();
       const elapsed = (time - startTime) / duration;
       const value = ease(elapsed > 1 ? 1 : elapsed);
-      onTick(value);
+      tick(value);
       if (elapsed >= 1) {
         window.cancelAnimationFrame(frame);
         resolve();
